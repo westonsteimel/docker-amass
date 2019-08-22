a2cc994a4568f3a62b42c5b249281dccbc64c66c
Weston Steimel <weston.steimel@gmail.com>
2019-08-22 23:32:34 +0100

    switch to GitHub docker tag action now that inspect works
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- stable/tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VERSION=$(grep -e "ARG AMASS_VERSION=" stable/Dockerfile)
-VERSION=${VERSION#ARG AMASS_VERSION=\"}
-VERSION=${VERSION%\"}
-echo "Tagging version ${VERSION}"
-docker tag "${DOCKER_USERNAME}/amass:latest" "${DOCKER_USERNAME}/amass:${VERSION}"
